475b90e0d8b7ab9f3f141aa60395d90629b182c4
fpupox <64444703+fpupox@users.noreply.github.com>
2024-06-17 05:38:43 +0000

    CHECKING JUNE

--- src/components/Imaging.jsx
+++ src/components/Imaging.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Imaging = ({ src, alt, ...props }) => {
-  return <img src={src} alt={alt} {...props} />;
+  return <Image src={src} alt={alt} {...props} />;
 };
 
 export default Imaging;
--- src/services/getProfilesById.jsx
+++
@@ -1,7 +0,0 @@
-export async function getProfileById (id) {
-    const response = await fetch(
-        `https://supademo.ernestofreyre.com/api/profiles/$(id)`
-    );
-    const data = await response.json();
-    return data;
-}
